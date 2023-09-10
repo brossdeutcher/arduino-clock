d0beb4571ab6c83f9c2ed07bb984b2c969c8b63c
brossdeutcher <113945283+brossdeutcher@users.noreply.github.com>
2023-09-10 14:34:47 -0600

    Add files via upload

---
+++ alarm-clock.ino
@@ -0,0 +1,106 @@
+#include <RTClib.h>
+#include <TM1637Display.h>
+
+
+// Red LED
+#define redLED 3
+
+// Rotary Encoder
+#define rotarySW 8
+#define rotaryDT 9
+#define rotaryCLK 10
+int rotaryCounter = 0;
+int currentStateRotaryCLK;
+int lastStateRotaryCLK;
+String rotaryCurrentDir = "";
+unsigned long rotaryLastButtonPress = 0;
+
+// Real-Time Clock DS1307
+// #define rtcSDA SDA
+// #define rtcSCL SCL
+// #define rtcSQW 10
+RTC_DS1307 rtc;
+DateTime now;
+
+// Seven-Segment Display
+#define tm1637DIO 5
+#define tm1637CLK 6
+TM1637Display sevSegDisplay = TM1637Display(tm1637CLK, tm1637DIO);
+const uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+const uint8_t all_on[] = { 0xff, 0xff, 0xff, 0xff };
+const uint8_t alrm[] = {
+  SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,
+  SEG_D | SEG_E | SEG_F,
+  SEG_A | SEG_E | SEG_F,
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G
+};
+
+
+void setup() {
+  // Red LED
+  pinMode(redLED, OUTPUT);
+
+  // Seven Segment Display
+  sevSegDisplay.clear();
+  sevSegDisplay.setBrightness(7);
+  sevSegDisplay.clear();
+  sevSegDisplay.setSegments(alrm);
+  delay(1000);
+
+  // Rotary Encoder
+  pinMode(rotarySW, INPUT_PULLUP);
+  pinMode(rotaryDT, INPUT);
+  pinMode(rotaryCLK, INPUT);
+  Serial.begin(9600);
+  lastStateRotaryCLK = digitalRead(rotaryCLK);
+  
+  // RTC
+  rtc.begin();
+  rtc.adjust(DateTime(2023, 9, 7, 20, 58, 5)); // this is where you adjust the start time
+}
+
+void loop() {
+  now = rtc.now();
+
+  sevSegHandler();
+  rotaryEncoderHandler();
+  
+  // TODO: add alarm functionality
+  // alarm();
+
+}
+
+void rotaryEncoderHandler() {
+  
+  // TODO: fix rotary encoder (sometimes mistakes which direction it is going)
+  // TODO: add/subtract minutes with rotary encoder
+  // TODO: set alarm with rotary encoder
+  
+  currentStateRotaryCLK = digitalRead(rotaryCLK);
+  if (currentStateRotaryCLK != lastStateRotaryCLK && currentStateRotaryCLK == 1) {
+    if (digitalRead(rotaryDT) != currentStateRotaryCLK) {
+      // min--;
+      Serial.println("min ++");
+    } else {
+      // min++;
+      Serial.println("min --");
+    }
+  }
+  lastStateRotaryCLK = currentStateRotaryCLK;
+}
+
+void sevSegHandler() {
+  // int hrMin = (hrs * 100) + min;
+  int hrMin = (now.hour() * 100) + now.minute();
+  sevSegDisplay.showNumberDec(hrMin);
+}
+
+void alarm() {
+  digitalWrite(redLED, HIGH);
+  delay(500);
+  digitalWrite(redLED, LOW);
+  delay(750);
+
+  // TODO: add sound
+
+}
