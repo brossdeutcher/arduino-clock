a630b6a235ace5098ca77ca35b61a6537225f5f9
brossdeutcher <113945283+brossdeutcher@users.noreply.github.com>
2023-10-13 19:32:03 -0600

    shows colon in time

--- arduino-clock.ino
+++ arduino-clock.ino
@@ -4,10 +4,8 @@
 
 // Red LED
 #define redLED 3
-
 // buzzer
 #define buzzer 4
-
 // Rotary Encoder
 #define rotarySW 8
 #define rotaryDT 9
@@ -17,11 +15,9 @@ int currentStateRotaryCLK;
 int lastStateRotaryCLK;
 String rotaryCurrentDir = "";
 unsigned long rotaryLastButtonPress = 0;
-
 // Real-Time Clock DS1307
 RTC_DS1307 rtc;
 DateTime now;
-
 // Seven-Segment Display
 #define tm1637DIO 5
 #define tm1637CLK 6
@@ -39,21 +35,18 @@ const uint8_t alrm[] = {
 void setup() {
   // Red LED
   pinMode(redLED, OUTPUT);
-
   // Seven Segment Display
   sevSegDisplay.clear();
   sevSegDisplay.setBrightness(7);
   sevSegDisplay.clear();
   sevSegDisplay.setSegments(alrm);
   delay(1000);
-
   // Rotary Encoder
   pinMode(rotarySW, INPUT_PULLUP);
   pinMode(rotaryDT, INPUT);
   pinMode(rotaryCLK, INPUT);
   Serial.begin(9600);
   lastStateRotaryCLK = digitalRead(rotaryCLK);
-  
   // RTC
   rtc.begin();
   rtc.adjust(DateTime(2023, 9, 7, 21, 6, 5)); // this is where you adjust the start time
@@ -64,9 +57,7 @@ void loop() {
 
   sevSegHandler();
   rotaryEncoderHandler();
-  
-  // TODO: add alarm functionality
-  // alarm();
+
   if (now.hour() == 8 && now.minute() == 0 && now.second() < 10) {
     alarm();
   }
@@ -95,11 +86,14 @@ void rotaryEncoderHandler() {
 }
 
 void sevSegHandler() {
-  int hrMin = (now.hour() * 100) + now.minute();
-  sevSegDisplay.showNumberDec(hrMin);
+  sevSegDisplay.showNumberDecEx(now.minute(), 0, true, 2, 2);
+  sevSegDisplay.showNumberDecEx(now.hour(), 0b01000000, true, 2, 0);
 }
 
 void alarm() {
+
+  // TODO: flash light & play song at same time
+
   digitalWrite(redLED, HIGH);
   delay(500);
   digitalWrite(redLED, LOW);
